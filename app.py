474755e8a946b59402f1e64cc286a87e32a7cbda
firestorm-buckman <94107055+firestorm-buckman@users.noreply.github.com>
2022-02-09 21:22:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,17 +54,49 @@ server = app.server
 app.title=tabtitle
 
 ########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
+df = px.data.tips()# Build App
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
+#app = dcc.Dash(__name__)
+app.title = "Avocado Analytics: Understand Your Avocados!"
+
+app.layout =  html.Div(
+    children=[
+        html.H1(children="Avocado Analytics",),
+        html.P(
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
+            " between 2015 and 2018",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": [1, 2, 3],
+                        "y": [2, 3, 4],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": [1, 2, 3],
+                        "y": [1, 3, 5],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
     ]
 )
+
+#app.css.config.serve_locally = True
+#app.scripts.config.serve_locally = True
 
 if __name__ == '__main__':
     app.run_server()
